f707443063481df5786c3548810139ef8c3e918c
Jaša <78748619+jasasonc@users.noreply.github.com>
2026-06-12 12:59:58 +0200

    Curate the public API per the sdypy public-api contract (SEP 2)

--- sdypy/excitation/__init__.py
+++ sdypy/excitation/__init__.py
@@ -9,4 +9,6 @@ try:
 except PackageNotFoundError:  # source checkout without installed metadata
     __version__ = "0+unknown"
 
-from pyExSi import *
+from pyExSi import (burst_random, get_kurtosis, get_psd, impulse, nonstationary_signal, normal_random, pseudo_random, random_gaussian, sine_sweep, stationary_nongaussian_signal, uniform_random)
+
+__all__ = ["burst_random", "get_kurtosis", "get_psd", "impulse", "nonstationary_signal", "normal_random", "pseudo_random", "random_gaussian", "sine_sweep", "stationary_nongaussian_signal", "uniform_random"]
